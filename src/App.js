ad0500ef7284b6c05d119c46f8e3ef08bca35cf2
SDS0008 <sdivyasumanth008@gmail.com>
2024-07-17 20:15:06 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import '../src/components/CardItem/index.css';
+
+import './App.css'
+
+import CardItem from './components/CardItem';
+
+
+const cardsList = [
+
+  {
+    id: 1,
+    title: 'Data Scientist',
+    description: 'Data scientist ghj ghnjkm dfghjk dfghjkjhgf dfghjk 0',
+    imgUrl: 'https://tse1.mm.bing.net/th?id=OIP.8saFYrJpmYxzt6g2e_dOZwHaE8&pid=Api&P=0&h=220',
+    className: ' cards card-1',
+  },
+
+  {
+    id: 2,
+    title: 'IOT Developer',
+    description: ' IOT Developer ghj ghnjkm dfghjk dfghjkjhgf dfghjk 0',
+    imgUrl: 'https://tse1.mm.bing.net/th?id=OIP.39biKD21YoKjeFfpRcVITwHaHa&pid=Api&P=0&h=220',
+    className: ' cards card-2',
+  },
+
+  {
+    id: 3,
+    title: 'Vr DEV',
+    description: 'Vr DEV ghj ghnjkm dfghjk dfghjkjhgf dfghjk 0',
+    imgUrl: 'https://tse1.mm.bing.net/th?id=OIP.pbW6WaD_cpaaRGz_dCk5GAHaEL&pid=Api&P=0&h=220',
+    className: 'cards card-3',
+  },
+
+  {
+    id: 4,
+    title: 'HL Engineering',
+    description: 'HL Engineering ghj ghnjkm dfghjk dfghjkjhgf dfghjk 0',
+    imgUrl: 'https://tse4.mm.bing.net/th?id=OIP.THTlx8NYjWG_Q8VvfDKRqQHaDL&pid=Api&P=0&h=220',
+    className: ' cards card-4',
+  },
+
+
+
+]
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="technologies-bg-con">
+      <h1 className="technologies-heading">
+        Learn 4.0 technologies
+      </h1>
+
+      <p className="technologies-description">
+
+        Get trained by alumiof IITS dxcfvgbhmk ghvhbl gl fghcsdbsdjbj bfhadajhh jjhsdhljfahj gj nkm.
+
+      </p>
+
+      <ul className='technologies-card-con'>
+
+        { cardsList.map(eachObj => (
+          <CardItem sourceObj={ eachObj } key={ eachObj.id } />
+        )) }
+      </ul>
+
     </div>
   );
 }
